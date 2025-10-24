71127a7e71cefb0a922cda6a82749bbaa4616daf
joshu <joshuajackargent@hotmail.com>
2025-10-24 14:39:16 +0100

    Fix deployment issues and add scenes
    Ensures deployment is successful

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,7 @@
-import * as dotenv from "dotenv";
-dotenv.config();
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import * as schema from './schema';
 
-import { neon } from "@netlify/neon";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-
-export const db = drizzle({
-  client: neon(process.env.NETLIFY_DATABASE_URL!),
-  schema,
-});
-
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
